56fcfc5d0af5bbff73a5a6a31b95145eb4a6a7c6
badampudimahesh2005 <120025054+badampudimahesh2005@users.noreply.github.com>
2023-10-30 02:00:47 +0530

    Create lecture2.cpp

---
+++ lecture2.cpp
@@ -0,0 +1,31 @@
+
+
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+    {     
+        sort(A,A+n);
+        for(int i=0;i<n;i++){
+            int f=X-A[i];
+            int left=i+1;
+            int right=n-1;
+            
+            while(left<right){
+            if(A[left]+ A[right]==f){
+                return 1;
+            }else if(A[left]+A[right] >f){
+                right--;
+            }else if(A[left] + A[right] <f){
+                left++;
+            }
+        }
+        }
+        return 0;
+        
+        
+        
+    }
+
+};
